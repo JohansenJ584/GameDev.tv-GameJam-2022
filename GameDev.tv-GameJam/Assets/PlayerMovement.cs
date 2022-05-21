1ef33a13355fd5db23160fc9f91c91e0cf6587e9
JohansenJ584 <54958923+JohansenJ584@users.noreply.github.com>
2022-05-21 19:17:27 -0400

    Finshed Basic Movement and added doubleJump and wallJump

--- GameDev.tv-GameJam/Assets/PlayerMovement.cs
+++ GameDev.tv-GameJam/Assets/PlayerMovement.cs
@@ -8,15 +8,24 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private LayerMask wallLayer;
     private Rigidbody2D body;
     private BoxCollider2D boxCollider;
-    private float wallJumpCooldown;
+    private float JumpCooldown;
+    private float WallCooldown;
+    private float gripTime;
+    private float timeToDoubleJump;
     private float horizontalInput;
+    private float gravityScale;
     //private Animator anim;
+    [SerializeField] private bool canDoubleJump = false;
+    [SerializeField] private bool canWallJump = false;
+
+    
+    private bool didDoubleJump = false;
     
     private void Awake()
     {   
-        //Grabs references for rigidbody and animator from game object.
         body = GetComponent<Rigidbody2D>();
         boxCollider = GetComponent<BoxCollider2D>();
+        gravityScale = body.gravityScale;
         //anim = GetComponent<Animator>();
     }
  
@@ -24,35 +33,50 @@ public class PlayerMovement : MonoBehaviour
     {
         horizontalInput = Input.GetAxis("Horizontal");
         
-        //Flip player when facing left/right.
+        //Directional for animation and for sign mathf
         if (horizontalInput > 0.01f)
-            transform.localScale = new Vector3(Mathf.Abs(transform.localScale.x),  transform.localScale.y,  transform.localScale.z);
-        else if (horizontalInput < -0.01f)
-            transform.localScale = new Vector3(transform.localScale.x * -1,  transform.localScale.y,  transform.localScale.z);
-
-        Debug.Log(onWall());
-        if(wallJumpCooldown > 0.2f)
         {
-            body.velocity = new Vector2(horizontalInput * speed, body.velocity.y);
-            if(onWall() && !isGrounded())
+            transform.localScale = new Vector3(Mathf.Abs(transform.localScale.x),  transform.localScale.y,  transform.localScale.z);
+        }
+        else if (horizontalInput < -0.01f)
+        {
+            transform.localScale = new Vector3(transform.localScale.x * -1,  transform.localScale.y,  transform.localScale.z);
+        }
+
+            if(canWallJump && onWall() && !isGrounded() && gripTime < 1.00f && WallCooldown < 0)
             {
+                body.velocity = Vector2.zero;
                 body.gravityScale = 0;
                 body.velocity = Vector2.zero;
             }
             else
             {
-                body.gravityScale = 1;         //NEED A UTIL struct or something
+                if(isGrounded())
+                {
+                    gripTime = 0;
+                    timeToDoubleJump = 0;
+                }
+                body.gravityScale = gravityScale;
             }
-            if (Input.GetKey(KeyCode.Space))
+
+        if(JumpCooldown > 0.2f)
+        {
+            body.velocity = new Vector2(horizontalInput * speed, body.velocity.y);
+            if (Input.GetKeyDown(KeyCode.Space))
             {
                 Jump();
             }
         }
+        JumpCooldown += Time.deltaTime;
+        gripTime += Time.deltaTime;
+        WallCooldown -= Time.deltaTime;
+        timeToDoubleJump += Time.deltaTime;
+        /*
         else
         {
-            wallJumpCooldown += Time.deltaTime;
+            JumpCooldown += Time.deltaTime;
+            WallCooldown -= Time.deltaTime;
         }
- /*
         //sets animation parameters
         anim.SetBool("run", horizontalInput != 0);
         anim.SetBool("grounded", grounded);
@@ -63,21 +87,31 @@ public class PlayerMovement : MonoBehaviour
     {
         if(isGrounded())
         {
+            didDoubleJump = false;
             body.velocity = new Vector2(body.velocity.x, jumpPower);
             //anim.SetTrigger("jump");
         }
-        else if(onWall() && !isGrounded())
+        else if(canWallJump && onWall() && !isGrounded())
         {
+            /*
             if(horizontalInput == 0)
             {
-                body.velocity = new Vector2(-Mathf.Sign(transform.localScale.x) * 10, 0);
+                body.velocity = new Vector2(-Mathf.Sign(transform.localScale.x) * 1, 0);
                 transform.localScale = new Vector3(-Mathf.Sign(transform.localScale.x) * transform.localScale.x, transform.localScale.y,transform.localScale.z);
             }
             else
             {
-                body.velocity = new Vector2(-Mathf.Sign(transform.localScale.x) * 3, 6);
+                body.velocity = new Vector2(-Mathf.Sign(transform.localScale.x) * 3, 6) / 2;
             }
-            wallJumpCooldown = 0;
+            */
+            body.velocity = new Vector2(-Mathf.Sign(transform.localScale.x) * 3, jumpPower);
+            WallCooldown = 3.0f;
+            JumpCooldown = 0;
+        }
+        else if(canDoubleJump && !didDoubleJump && timeToDoubleJump > .25f && !onWall() && !isGrounded())
+        {
+            didDoubleJump = true;
+            body.velocity = new Vector2(body.velocity.x, jumpPower * .8f);
         }
     }
  
@@ -91,10 +125,10 @@ public class PlayerMovement : MonoBehaviour
         return raycastHit.collider != null;
     } 
 
-        private bool onWall()
+    private bool onWall()
     {
         RaycastHit2D raycastHit = Physics2D.BoxCast(boxCollider.bounds.center, boxCollider.bounds.size, 0, new Vector2(transform.localScale.x, 0), 0.1f, wallLayer);
-        return raycastHit.collider != null;
+        return raycastHit.collider != null && WallCooldown < 0;
     } 
 
 }
